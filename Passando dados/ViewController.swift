33b995e2003d717d08f2eb1bf6836e6bf7d9fafe
gilmar <jiraspiom@yahoo.com.br>
2018-10-24 11:24:14 -0300

    Initial Commit

---
+++ Passando dados/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Passando dados
+//
+//  Created by Gilmar Borges on 24/10/18.
+//  Copyright © 2018 alalgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
